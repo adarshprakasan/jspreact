232bfd8fff01fa4e65e20bf2b5d2bb935bed404d
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-09-27 00:44:32 +0530

    refreshing repo

--- thirdApp/src/Songs.jsx
+++ thirdApp/src/Songs.jsx
@@ -1,24 +1,74 @@
-import Despacito from "./Despacito.mp3";
-import Dynamite from "./Dynamite.mp3";
-import LeftnRight from "./Left and Right Feat Jung Kook of BTS.mp3";
-import MyUniverse from "./My Universe.mp3";
+import EightLetter from "./Songs/8 Letters.mp3";
+import EightLettersCover from "./Songs/8lettersCover.jpeg";
+import Dandelions from "./Songs/Dandelions.mp3";
+import DandelionsCover from "./Songs/dandelionsCover.jpg";
+import Despacito from "./Songs/Despacito.mp3";
+import DespacitoCover from "./Songs/despacitoCover.png";
+import Dynamite from "./Songs/Dynamite.mp3";
+import DynamiteCover from "./Songs/dynamitCover.png";
+import LeftnRight from "./Songs/Left and Right Feat Jung Kook of BTS.mp3";
+import LnrCover from "./Songs/lnrCover.jpg";
+import MakeItRight from "./Songs/Make It Right feat Lauv.mp3";
+import MakeItRightCover from "./Songs/makeitrightCover.png";
+import Money from "./Songs/MONEY.mp3";
+import MoneyCover from "./Songs/moneyCover.jpg";
+import MyUniverse from "./Songs/My Universe.mp3";
+import MyUniCover from "./Songs/myUniCover.jpg";
+import Senorita from "./Songs/Señorita.mp3";
+import SenoritaCover from "./Songs/senoritaCover.jpg";
+import ShapeOfYou from "./Songs/Shape of You.mp3";
+import ShapeOfYouCover from "./Songs/shapeofyouCover.png";
 
 let songs = [
   {
+    cover: EightLettersCover,
+    title: "8 Letter",
+    src: EightLetter,
+  },
+  {
+    cover: DandelionsCover,
+    title: "Dandelions",
+    src: Dandelions,
+  },
+  {
+    cover: DespacitoCover,
     title: "Despacito",
     src: Despacito,
   },
   {
+    cover: DynamiteCover,
     title: "Dynamite",
     src: Dynamite,
   },
   {
+    cover: LnrCover,
     title: "Left and Right",
     src: LeftnRight,
   },
   {
+    cover: MakeItRightCover,
+    title: "Make it Right",
+    src: MakeItRight,
+  },
+  {
+    cover: MoneyCover,
+    title: "Money",
+    src: Money,
+  },
+  {
+    cover: MyUniCover,
     title: "My Universe",
     src: MyUniverse,
+  },
+  {
+    cover: SenoritaCover,
+    title: "Señorita",
+    src: Senorita,
+  },
+  {
+    cover: ShapeOfYouCover,
+    title: "Shape of You",
+    src: ShapeOfYou,
   },
 ];
 
--- thirdApp/src/main.jsx
+++ thirdApp/src/main.jsx
@@ -1,5 +1,10 @@
 import React, { useRef, useState } from "react";
 import songs from "./Songs";
+import "./main.css";
+import { IoPlayCircleOutline } from "react-icons/io5";
+import { FaRegCirclePause } from "react-icons/fa6";
+import { IoPlaySkipBackCircleOutline } from "react-icons/io5";
+import { IoPlaySkipForwardCircleOutline } from "react-icons/io5";
 
 function Main() {
   let [play, setPlay] = useState(false);
@@ -38,35 +43,49 @@ function Main() {
   };
 
   return (
-    <div>
-      <audio
-        src={currentSong.src}
-        ref={audioRef}
-        onTimeUpdate={timeUpdateHandler}
-        onEnded={() => {
-          skipForwardPlayHandler("skip-forward");
-        }}
-      ></audio>
+    <div className="mainBody">
+      <div className="player">
+        <audio
+          src={currentSong.src}
+          ref={audioRef}
+          onTimeUpdate={timeUpdateHandler}
+          onEnded={() => {
+            skipForwardPlayHandler("skip-forward");
+          }}
+        ></audio>
 
-      <h1>Title: {currentSong.title}</h1>
+        <div className="cover">
+          <img src={currentSong.cover} alt="" />
+        </div>
 
-      <div>
-        <input
-          type="range"
-          value={currentTime}
-          max={audioRef.current ? audioRef.current.duration : 0}
-          onChange={dragHandler}
-        />
-      </div>
+        <div className="songTitle">
+          <h1>{currentSong.title}</h1>
+        </div>
 
-      <div>
-        <button onClick={() => skipForwardPlayHandler("skip-backward")}>
-          Back
-        </button>
-        <button onClick={playOrPause}>{play ? "Pause" : "Play"}</button>
-        <button onClick={() => skipForwardPlayHandler("skip-forward")}>
-          Forward
-        </button>
+        <div>
+          <input
+            type="range"
+            value={currentTime}
+            max={audioRef.current ? audioRef.current.duration : 0}
+            onChange={dragHandler}
+          />
+        </div>
+
+        <div className="controls">
+          <button onClick={() => skipForwardPlayHandler("skip-backward")}>
+            <IoPlaySkipBackCircleOutline className="buttonStyles" />
+          </button>
+          <button onClick={playOrPause}>
+            {play ? (
+              <FaRegCirclePause className="mainButtonStyles" />
+            ) : (
+              <IoPlayCircleOutline className="mainButtonStyles" />
+            )}
+          </button>
+          <button onClick={() => skipForwardPlayHandler("skip-forward")}>
+            <IoPlaySkipForwardCircleOutline className="buttonStyles" />
+          </button>
+        </div>
       </div>
     </div>
   );
