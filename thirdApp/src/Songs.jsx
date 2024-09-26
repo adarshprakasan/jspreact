351c6fb7dce49fdb2bdade8c110745a79083784a
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-09-26 17:00:05 +0530

    musicApp initialised

---
+++ thirdApp/src/Songs.jsx
@@ -0,0 +1,25 @@
+import Despacito from "./Despacito.mp3";
+import Dynamite from "./Dynamite.mp3";
+import LeftnRight from "./Left and Right Feat Jung Kook of BTS.mp3";
+import MyUniverse from "./My Universe.mp3";
+
+let songs = [
+  {
+    title: "Despacito",
+    src: Despacito,
+  },
+  {
+    title: "Dynamite",
+    src: Dynamite,
+  },
+  {
+    title: "Left and Right",
+    src: LeftnRight,
+  },
+  {
+    title: "My Universe",
+    src: MyUniverse,
+  },
+];
+
+export default songs;
--- thirdApp/src/main.jsx
+++ thirdApp/src/main.jsx
@@ -1,10 +1,75 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useRef, useState } from "react";
+import songs from "./Songs";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Main() {
+  let [play, setPlay] = useState(false);
+  let [currentTime, setCurrentTime] = useState(0);
+  let audioRef = useRef(null);
+
+  let [currentSongIndex, setCurrentSongIndex] = useState(0);
+  let currentSong = songs[currentSongIndex];
+
+  let playOrPause = () => {
+    if (play) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlay(!play);
+  };
+
+  let timeUpdateHandler = (e) => {
+    setCurrentTime(e.target.currentTime);
+  };
+
+  let dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setCurrentTime(e.target.value);
+  };
+
+  let skipForwardPlayHandler = (direction) => {
+    if (direction === "skip-forward") {
+      setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+    } else if (direction === "skip-backward") {
+      setCurrentSongIndex(
+        (prevIndex) => (prevIndex - 1 + songs.length) % songs.length
+      );
+    }
+  };
+
+  return (
+    <div>
+      <audio
+        src={currentSong.src}
+        ref={audioRef}
+        onTimeUpdate={timeUpdateHandler}
+        onEnded={() => {
+          skipForwardPlayHandler("skip-forward");
+        }}
+      ></audio>
+
+      <h1>Title: {currentSong.title}</h1>
+
+      <div>
+        <input
+          type="range"
+          value={currentTime}
+          max={audioRef.current ? audioRef.current.duration : 0}
+          onChange={dragHandler}
+        />
+      </div>
+
+      <div>
+        <button onClick={() => skipForwardPlayHandler("skip-backward")}>
+          Back
+        </button>
+        <button onClick={playOrPause}>{play ? "Pause" : "Play"}</button>
+        <button onClick={() => skipForwardPlayHandler("skip-forward")}>
+          Forward
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
