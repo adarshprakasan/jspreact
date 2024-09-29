dbade7a5fdb8465a0c18634e3503b771c0a75c06
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-09-29 17:08:35 +0530

    weather app css work

--- fourthapp/src/Components/WeatherApp.jsx
+++ fourthapp/src/Components/WeatherApp.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import WeatherCss from "../css/WeatherApp.module.css";
+import { FaTemperatureArrowDown } from "react-icons/fa6";
+import { FaTemperatureArrowUp } from "react-icons/fa6";
 
 function WeatherApp() {
   let [city, setCity] = useState("");
@@ -7,6 +10,7 @@ function WeatherApp() {
   let fetchApi = async () => {
     let apiKey = "b710e1b242363cc766f242a320cf5365";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    // let apiUrl = `https://google.com`;
 
     try {
       let data = await fetch(apiUrl);
@@ -23,22 +27,47 @@ function WeatherApp() {
   };
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter the city name"
-        onChange={(e) => {
-          setCity(e.target.value);
-        }}
-      />
-      <button onClick={fetchApi}>Get Weather</button>
-      <div>
+      <div className={WeatherCss.searchHeader}>
+        <input
+          type="text"
+          placeholder="Enter the city name"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button onClick={fetchApi}>Get Weather</button>
+      </div>
+      <div className={WeatherCss.resultSection}>
         {weatherInfo && (
-          <>
-            <h1>City Name : {weatherInfo.name}</h1>
-            <h2>Temperature : {weatherInfo.main.temp}</h2>
-            <h2>Country : {weatherInfo.sys.country}</h2>
-            <h2>Weather Description : {weatherInfo.weather[0].description}</h2>
-          </>
+          <div className={WeatherCss.searchResult}>
+            <div className={WeatherCss.cardTop}>
+              <div className={WeatherCss.cityDetails}>
+                <h1>{weatherInfo.name}</h1>
+                <h6>
+                  <i>{weatherInfo.sys.country}</i>
+                </h6>
+                <p>
+                  Lat : {weatherInfo.coord.lat}
+                  <span /> | Long : {weatherInfo.coord.lon}
+                </p>
+              </div>
+              <div className={WeatherCss.weatherDetails}>
+                <h2>{weatherInfo.main.temp}°C</h2>
+                <h4>{weatherInfo.weather[0].description}</h4>
+                <p>
+                  <FaTemperatureArrowDown /> : {weatherInfo.main.temp_min}°C{" "}
+                  &nbsp; <span /> | &nbsp;
+                  <FaTemperatureArrowUp /> : {weatherInfo.main.temp_max}°C
+                </p>
+              </div>
+            </div>
+            <div className={WeatherCss.cardBottom}>
+              <p>
+                Humidity : {weatherInfo.main.humidity}
+                &nbsp; <span /> | &nbsp; Pressure : {weatherInfo.main.pressure}
+              </p>
+            </div>
+          </div>
         )}
       </div>
     </div>
